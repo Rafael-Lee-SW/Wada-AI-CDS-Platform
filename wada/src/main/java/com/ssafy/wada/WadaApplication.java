3c497e6f383e7fc5169662ace49b50ebd0192f58
김유석 <itgorae.seok@gmail.com>
2024-10-25 15:11:13 +0900

    Add initial project files and setup

---
+++ wada/src/main/java/com/ssafy/wada/WadaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.wada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WadaApplication.class, args);
+	}
+
+}
