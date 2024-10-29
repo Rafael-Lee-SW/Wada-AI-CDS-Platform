6b122923d5b0e904d939c7d0ca22934ad577eb04
김유석 <itgorae.seok@gmail.com>
2024-10-29 12:15:38 +0900

    [BE] feat: csv 데이터 파싱 로직 구현

--- backend/src/main/java/com/ssafy/wada/common/error/CsvParsingErrorCode.java
+++ backend/src/main/java/com/ssafy/wada/common/error/CsvParsingErrorCode.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 public enum CsvParsingErrorCode implements ErrorCode {
 
 	CSV_PARSING_ERROR(HttpStatus.BAD_REQUEST, "CSV파일을 읽는데 실패했습니다.", LogLevel.INFO),
-	JSON_PROCESSING_ERROR(HttpStatus.BAD_REQUEST, "잘못된 요청값입니다.", LogLevel.WARN),
+	JSON_PROCESSING_ERROR(HttpStatus.BAD_REQUEST, "JSON 변환 중 오류가 발생했습니다.", LogLevel.WARN),
 	INVALID_CSV_DATA(HttpStatus.BAD_REQUEST, "CSV파일에 유효하지 않은 값이 포함되어 있습니다.", LogLevel.INFO),
 	INVALID_HEADER(HttpStatus.BAD_REQUEST, "CSV파일 헤더가 유효하지 않습니다.", LogLevel.INFO);
 
--- backend/src/main/java/com/ssafy/wada/presentation/controller/MLModelRecommendController.java
+++ backend/src/main/java/com/ssafy/wada/presentation/controller/MLModelRecommendController.java
@@ -2,7 +2,9 @@ package com.ssafy.wada.presentation.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.wada.application.service.MLRecommendationService;
 import com.ssafy.wada.presentation.response.MLRecommendResponse;
@@ -17,7 +19,9 @@ public class MLModelRecommendController {
 	private final MLRecommendationService mlRecommendationService;
 
 	@PostMapping
-	public MLRecommendResponse getMLModelRecommend() {
-		return mlRecommendationService.recommend();
+	public MLRecommendResponse getMLModelRecommend(
+		@RequestParam("file") MultipartFile file,
+		@RequestParam("chatId") String chatId) {
+		return mlRecommendationService.recommend(file, chatId);
 	}
 }
