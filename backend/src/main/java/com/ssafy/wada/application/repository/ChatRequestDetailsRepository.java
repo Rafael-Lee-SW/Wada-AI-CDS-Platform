a7ddf4a8efabd13215602f386263fa5f0fdfc6c6
김유석 <itgorae.seok@gmail.com>
2024-11-04 15:20:42 +0900

    [BE] feat: update ModelDispatchService

--- backend/src/main/java/com/ssafy/wada/application/repository/ChatRequestDetailsRepository.java
+++ backend/src/main/java/com/ssafy/wada/application/repository/ChatRequestDetailsRepository.java
@@ -1,18 +1,20 @@
 package com.ssafy.wada.application.repository;
 
 import com.ssafy.wada.application.domain.ChatRequestDetails;
-import com.ssafy.wada.application.domain.RecommendedLLM;
+import java.util.Map;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface ChatRequestDetailsRepository extends MongoRepository<ChatRequestDetails, String> {
 
     Optional<ChatRequestDetails> findById(String requestId);
 
-    ChatRequestDetails findByRequestId(String requestId);
+    @Query(value = "{ '_id': ?0 }", fields = "{ 'recommendedLLM': 1, '_id': 0 }")
+    Map<String, Object> findRecommendedLLMById(String requestId);
 
-    ChatRequestDetails findFileUrlByRequestId(String requestId);
 
-
+    @Query(value = "{ '_id': ?0 }", fields = "{ 'fileUrl': 1 }")
+    Optional<String> findFileUrlByRequestId(String requestId);
 }
